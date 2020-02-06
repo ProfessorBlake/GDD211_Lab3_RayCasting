d01a757f335ca414a3ac24c3db2d9fc3e4ac23b6
Blake <telefragent@gmail.com>
2020-02-06 16:22:40 -0500

    Initial commit

---
+++ Assets/1. Raycasting Intro/Scripts/IntroPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroPlayer : MonoBehaviour
+{
+	public SpriteRenderer Sprite;
+
+	private void Update()
+	{
+		//Rotate
+		transform.eulerAngles += new Vector3(0f, 0f, Time.deltaTime * 25f);
+
+		//Draw our ray
+		
+		RaycastHit2D hit = Physics2D.Raycast(transform.position,transform.up);
+
+		if(hit)
+		{
+			Debug.DrawLine(transform.position, hit.point, Color.red);
+			Sprite.color = Color.red;
+		}
+		else
+		{
+			Debug.DrawRay(transform.position, transform.up * 10f, Color.green);
+			Sprite.color = Color.green;
+		}
+	}
+
+
+}
